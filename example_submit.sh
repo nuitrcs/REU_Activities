1b59789cf668de4a8d7aa209a3bff6d58a427da8
svangenderen <144258576+svangenderen@users.noreply.github.com>
2024-06-10 13:05:06 -0500

    Create example_submit.sh

---
+++ example_submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=w10001  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=w10001  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
+#SBATCH --time=00:10:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=sample_job  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=outlog ## standard out and standard error goes to this file
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
+#SBATCH --mail-user=email@u.northwestern.edu ## your email
+
+module purge all
+module load python-anaconda3
+eval "$(conda shell.bash hook)"
+source activate slurm-py37-test
+
+python --version
+python slurm_test.py
