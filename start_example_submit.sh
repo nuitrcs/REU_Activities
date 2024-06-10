e461aa6acd834fcfea66a8117451c5d37051ab66
svangenderen <144258576+svangenderen@users.noreply.github.com>
2024-06-10 13:41:16 -0500

    Update and rename example_submit.sh to start_example_submit.sh

---
+++ start_example_submit.sh
@@ -0,0 +1,12 @@
+#!/
+#SBATCH 
+# finish the rest
+
+module ... #get rid of any modules in case you previosuly loaded some
+
+module load python-anaconda3
+eval "$(conda shell.bash hook)"
+source activate slurm-py37-test
+
+python #find out what version of python you are using
+python #call on the submission script
