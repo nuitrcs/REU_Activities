1a295873a95813887b4f0ca6724ea6326a72adc4
ablock79 <142450663+ablock79@users.noreply.github.com>
2025-06-25 09:47:29 -0500

    Update start_example_submit.sh

--- start_example_submit.sh
+++ start_example_submit.sh
@@ -5,9 +5,8 @@
 module ... #get rid of any modules in case you previosuly loaded some
 
 #load the modules
-module load python-anaconda3
-eval "$(conda shell.bash hook)"
-source activate slurm-py37-test
+module load python/3.12.10
+
 
 python #find out what version of python you are using
 python #call on the submission script
